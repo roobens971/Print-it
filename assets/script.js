dd41e92c22df2beadfc4b76e0fc2f61803e803f2
roobens landre <roobens971@gmail.com>
2025-01-20 14:06:32 +0100

    Ajout des images

--- assets/script.js
+++ assets/script.js
@@ -25,4 +25,28 @@ console.log(arrow);
 const bulletPoints = document.querySelector('.dots');
 console.log(bulletPoints);
 
+/*slide1*/
+const slide1 = document.querySelector(".slide1");
+console.log(slide1);
+slide1.src = "./assets/images/slideshow/slide1.jpg";
+slide1.classList.add("dot_selected");
 
+/*slide2*/
+const slide2 = document.querySelector(".slide2");
+console.log(slide2);
+slide2.src = "./assets/images/slideshow/slide2.jpg";
+/*slide2.classList.add("dot_selected");*/
+
+
+/*slide3*/
+const slide3 = document.querySelector(".slide3");
+console.log(slide3);
+slide3.src = "./assets/images/slideshow/slide3.jpg";
+/*slide3.classList.add("dot_selected");*/
+
+
+/*slide4*/
+const slide4 = document.querySelector(".slide4");
+console.log(slide4);
+slide4.src = "./assets/images/slideshow/slide4.jpg";
+/*slide4.classList.add("dot_selected");*/
